70972e88dc89aa2b829be36ed19374f7daf80594
kvydhyam <kiranvydh@gmail.com>
2024-12-01 07:57:36 -0800

    Iteration 1

--- Basic/Factorial.py
+++ Basic/Factorial.py
@@ -1,10 +1,11 @@
-num = int(input("Enter a number:"))
+num = int(input("Enter a number to calculate factorial"))
 factorial = 1
-if num < 0:
-    print("Factorial does not exist for negative numbers")
-elif num == 0:
-    print("Factorial of a 0 is 1 ")
+if num == 0:
+    print(f"Factorial of {num} is 1")
 else:
     for i in range(1,num+1):
-        factorial = factorial*i
-    print(f"The factorial of {num} is {factorial}")
+        factorial = factorial * i
+        print(factorial)
+    print(f"Factorial of {num} is {factorial}")
+
+
